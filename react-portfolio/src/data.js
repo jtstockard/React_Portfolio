ff4d473aef12e464f288ebdec498a1781efbeba2
jtstockard <jtstockard92@gmail.com>
2021-08-02 20:12:43 -0500

    inital react app created

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Delievery Done Right",
+    subtitle: "Front End API",
+    description:
+      "Delievery finding app that allows users to search for specific items and which resturants will deliver in your area",
+    image: "/img/portfolio/ddr.jpg",
+    link: "https://japinell.github.io/KU-CBC-Project-1-Delivery-Done-Right/",
+  },
+  {
+    title: "Trolley Turmoil",
+    subtitle: "Full Stack",
+    description:
+      "A user based reaction site that logs your input and allows users to create new additional prompts to appear on the homepage",
+    image: "img/portfolio/trolley.jpg",
+    link: "https://ku-cbc-project-2-beardbros.herokuapp.com/",
+  },
+  {
+    title: "Tech Blog",
+    subtitle: "MySQL and Heroku",
+    description:
+      "A user based blog post website that allows you to link email and social media accounts",
+    image: "img/portfolio/blog.jpg",
+    link: "https://tech-blog-jtstockard.herokuapp.com/",
+  },
+  {
+    title: "Note Taker",
+    subtitle: "MySQL and localStorage",
+    description:
+      "Saves all notes that would be required for your server and allows you to use CRUD operations",
+    image: "img/portfolio/note.jpg",
+    link: "https://note-taker-jtstockard.herokuapp.com/",
+  },
+];
